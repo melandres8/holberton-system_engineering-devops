2d7cab8e33a2a750117af41ef9c465ba9db23b69
Melkin Mosquera <melkinmosquera@Melkins-MacBook-Air.local>
2020-09-14 16:03:29 -0500

    task#0: improving

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,13 +15,15 @@ def TODO(user_id):
     url = 'https://jsonplaceholder.typicode.com'
     users_url = "{}/users/{}".format(url, user_id)
     EMPLOYEE_NAME = requests.get(users_url).json().get('name')
-    all_tasks = requests.get(f'{url}/todos?userId={user_id}').json()
-    completed = f'{url}/todos?userId={user_id}&completed=true'
+    all_tasks = requests.get('{}/todos?userId={}'.format(url, user_id)).json()
+    completed = '{}/todos?userId={}&completed=true'.format(url, user_id)
     done_tasks = requests.get(completed).json()
 
     tasks_size = len(all_tasks)
     done_len = len(done_tasks)
-    print(f"Employee {EMPLOYEE_NAME} is done with ({done_len}/{tasks_size}):")
+    print("Employee {} is done with ({}/{}):\
+    ".format(EMPLOYEE_NAME, done_len, tasks_size))
+
     for task in done_tasks:
         TASK_TITLE = task.get('title')
         print("\t " + TASK_TITLE)
